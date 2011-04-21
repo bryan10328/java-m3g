514208d756fc52638c8f681619e67dbf66f4a668
tueda@wolf.dog.cx <tueda@wolf.dog.cx@0c4e7430-8113-5b25-37bd-7ff228cca19c>
2011-04-21 02:51:17 +0000

    Java-M3Gを外部ライブラリからワークスペース内のプロジェクトに依存するように変更

--- trunk/build/android/Sample-01-simple/src/org/karlsland/m3g/M3GRenderer.java
+++ trunk/build/android/Sample-01-simple/src/org/karlsland/m3g/M3GRenderer.java
@@ -8,67 +8,64 @@ import android.util.Log;
 
 public class M3GRenderer implements Renderer {
 
-    
 	private World      wld;
 	private Graphics3D g3d;
 	private int        frame;
-	
+
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		if (frame++ % 100 == 0) {
-			Log.println(Log.INFO, "M3GRenderer", "onDrawFrame, i="+frame);
+			Log.println(Log.INFO, "M3GRenderer", "onDrawFrame, i=" + frame);
 		}
-//       g3d.render (wld);
+		g3d.render(wld);
 	}
-	
-	
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		Log.println(Log.INFO, "M3GRenderer", "onSurfaceChanged");
-//        g3d.setViewport (0, 0, width, height);
-//        Camera cam = wld.getActiveCamera ();
-//		String s = cam.getClass().getName();
-//       cam.setPerspective (45, width/(float)height, 0.1f, 100.f);
+		g3d.setViewport(0, 0, width, height);
+		Camera cam = wld.getActiveCamera();
+		String s = cam.getClass().getName();
+		cam.setPerspective(45, width / (float) height, 0.1f, 100.f);
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		Log.println(Log.INFO, "M3GRenderer", "onSurfaceCreated");
 		g3d = Graphics3D.getInstance();
-	    wld = new World();
-		
-		  	VertexArray positions         = new VertexArray (4, 3, 2);
-		  	short        positions_value[] = {1,-1,0, 1,1,0, -1,-1,0, -1,1,0};
-		    positions.set (0, 4, positions_value);
+		wld = new World();
 
-		    float scale   = 1;
-		    float[] bias = {0,0,0};
-		    VertexBuffer vertices = new VertexBuffer();
-		    vertices.setPositions (positions, scale, bias);
-		  
-		    int[] indices = {0,1,2,3};
-		    int[] stripLengths = {4};
+		VertexArray positions = new VertexArray(4, 3, 2);
+		short positions_value[] = { 1, -1, 0, 1, 1, 0, -1, -1, 0, -1, 1, 0 };
+		positions.set(0, 4, positions_value);
 
-		    TriangleStripArray tris = new TriangleStripArray (indices, stripLengths);
+		float scale = 1;
+		float[] bias = { 0, 0, 0 };
+		VertexBuffer vertices = new VertexBuffer();
+		vertices.setPositions(positions, scale, bias);
 
-		    Material mat = new Material();
-		    mat.setColor (Material.DIFFUSE, 0xff7f7fff);
+		int[] indices = { 0, 1, 2, 3 };
+		int[] stripLengths = { 4 };
 
-		    Appearance app = new Appearance();
-		    app.setMaterial (mat);
+		TriangleStripArray tris = new TriangleStripArray(indices, stripLengths);
 
-		    Mesh mesh = new Mesh (vertices, tris, app);
-		  
-		    Camera cam = new Camera();
-		    cam.translate (0,0,5);
+		Material mat = new Material();
+		mat.setColor(Material.DIFFUSE, 0xff7f7fff);
 
-		    Log.i("AAA", "cam="+cam);
-		    Log.i("AAA", "wld="+wld);
-		    wld.addChild (cam);
-		    Log.i("AAA", "wld="+wld);
-//		    wld.setActiveCamera (cam);
-//		    wld.addChild (mesh);
+		Appearance app = new Appearance();
+		app.setMaterial(mat);
+
+		Mesh mesh = new Mesh(vertices, tris, app);
+
+		Camera cam = new Camera();
+		cam.translate(0, 0, 5);
+
+		Log.i("AAA", "cam=" + cam);
+		Log.i("AAA", "wld=" + wld);
+		wld.addChild(cam);
+		Log.i("AAA", "wld=" + wld);
+		wld.setActiveCamera(cam);
+		wld.addChild(mesh);
 	}
 
 }
